d642776e340bdfd46a8fff6c1ff1e05aedfce827
Hernan Torres <129800629+Hernanjtg99@users.noreply.github.com>
2024-06-14 20:56:50 -0500

    InformacionLaboral.java

--- src/main/java/com/pruebatecnica/pruebatecnica/models/InformacionLaboral.java
+++ src/main/java/com/pruebatecnica/pruebatecnica/models/InformacionLaboral.java
@@ -12,7 +12,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "informacion laboral")
+@Table(name = "informacionLaboral")
 public class InformacionLaboral {
     
     @Column(name ="id_informacion_laboral")
