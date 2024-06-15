6eb79e2f6929d3eee0701829b689583d8775ad84
Hernan Torres <129800629+Hernanjtg99@users.noreply.github.com>
2024-06-14 20:57:13 -0500

    Update ReferenciaFamiliar.java

--- src/main/java/com/pruebatecnica/pruebatecnica/models/ReferenciaFamiliar.java
+++ src/main/java/com/pruebatecnica/pruebatecnica/models/ReferenciaFamiliar.java
@@ -9,7 +9,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "referencia familiar")
+@Table(name = "referenciaFamiliar")
 public class ReferenciaFamiliar {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
